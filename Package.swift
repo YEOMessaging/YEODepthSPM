c2bac640179a9a0f61a91056be7f3c8d18cff15f
paul calver <thecalver@gmail.com>
2026-03-24 10:23:49 +0000

    YEODepth 0.1.0 – update URL + checksum

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     .binaryTarget(
       name: "YEODepth",
       url: "https://github.com/YEOMessaging/YEODepthSPM/releases/download/0.1.0/YEODepth.xcframework.zip",
-      checksum: "3b14752a76045eeaa3e83fea7234e6ac7be6e4ef9792057f1b74d8aa14431e4b"
+      checksum: "1d8b9ebfdb52f329a5ca447966839ecaff3ae8f02a53635c86314f59b58afeab"
     )
   ]
 )
