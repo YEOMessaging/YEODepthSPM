39249ec02d4143178d363625c494b98439607177
paul calver <thecalver@gmail.com>
2026-03-24 12:37:20 +0000

    YEODepth 0.1.2 – update URL + checksum

--- Package.swift
+++ Package.swift
@@ -5,25 +5,14 @@ let package = Package(
   name: "YEODepth",
   platforms: [.iOS(.v15)],
   products: [
-    .library(name: "YEODepth", targets: ["YEODepth", "YEODepthPackageSupport"])
-  ],
-  dependencies: [
-    .package(url: "https://github.com/microsoft/onnxruntime-swift-package-manager", exact: "1.24.2")
+    .library(name: "YEODepth", targets: ["YEODepth"])
   ],
   targets: [
-    .target(
-      name: "YEODepthPackageSupport",
-      dependencies: [
-        .product(name: "onnxruntime", package: "onnxruntime-swift-package-manager")
-      ],
-      path: "Sources/YEODepthPackageSupport"
-    ),
     .binaryTarget(
       name: "YEODepth",
-      url: "https://github.com/YEOMessaging/YEODepthSPM/releases/download/0.1.1/YEODepth.xcframework.zip",
-      checksum: "e5fcb704d18da8cad86aea5c1b191377935f0b8a7bf09a823bacfe5af1f6acc2"
+      url: "https://github.com/YEOMessaging/YEODepthSPM/releases/download/0.1.2/YEODepth.xcframework.zip",
+      checksum: "5540d98fbd79cc34480e5497af157cb11dd4ef6df1f073ba8a138d1b3e49341e"
     )
   ]
 )
-
 
